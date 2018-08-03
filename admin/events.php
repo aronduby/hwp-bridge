edbe84147016187dbe64369a494b3225316dbacd
Aron Duby <aron.duby@gmail.com>
2018-08-02 23:12:02 -0400

    update the upcoming events page to view all per team

--- admin/events.php
+++ admin/events.php
@@ -5,30 +5,27 @@ if(!isset($_COOKIE['in'])){
 	die();
 }
 
-$upcoming_by_team = [
-	'V' => [],
-	'JV' => [] 
-];
+$upcomingLimit = 10;
 
 $schedule = new Schedule($season->id, PDODB::getInstance());
 $filtered = $schedule->getIterator();
-$upcoming = new \ScheduleFilter\DateRange($filtered, date('Y-m-d G:i:s', time()));
+
+$upcoming = new ScheduleFilter\DateRange($filtered, date('Y-m-d G:i:s', time()));
 $upcoming->rewind();
-if($upcoming->valid()){
 
-	$i=0;
-	foreach($upcoming as $next){
-		$type = ucwords($next->type);
-		$next = new $type($next->id, PDODB::getInstance());
-		$upcoming_by_team[$next->team][] = $next;
+$jv = new ScheduleFilter\Team($upcoming, 'JV');
+$jv->rewind();
 
-		$i++;
-		if($i == 10)
-			break;
-	}
+$varsity = new ScheduleFilter\Team($upcoming, 'V');
+$varsity->rewind();
 
-} else {
-	$upcoming_by_team = false;
+function printItems($iterator, $title) {
+    print '<li data-role="list-divider">'.$title.'</li>';
+    foreach($iterator as $next) {
+        $type = ucwords($next->type);
+        $next = new $type($next->id, PDODB::getInstance());
+        print $next->output('mobile-listing');
+    }
 }
 
 ?>
@@ -39,13 +36,15 @@ if($upcoming->valid()){
 	<title>Admin - Hudsonville Water Polo</title> 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+    <link rel="stylesheet" href="css/jquery-mobile-overrides.css" />
 	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
 	
 </head> 
 <body> 
 
-<div data-role="page" data-theme="b">
+<!-- Upcoming -->
+<div data-role="page" data-theme="b" id="soon">
 
 	<div data-role="header" data-theme="b">
 		<a href="index.php" title="back" data-icon="home" data-iconpos="notext" data-direction="reverse">back</a>
@@ -55,24 +54,78 @@ if($upcoming->valid()){
 
 	<div data-role="content">	
 		<?php
-		if($upcoming_by_team !==false){
+        if ($upcoming->valid()) {
+            print '<ul data-role="listview" data-theme="d" data-divider-theme="d" data-split-theme="d" data-split-icon="gear">';
 
-			print '<ul data-role="listview" data-theme="d" data-divider-theme="d" data-split-theme="d" data-split-icon="gear">';
-			foreach($upcoming_by_team as $team => $events){
-				print '<li data-role="list-divider">'.($team=='V' ? 'Varsity' : $team).'</li>';
-				foreach($events as $e){
-					print $e->output('mobile-listing');
-				}
-			}
-			print '</ul>';
+            if ($varsity->valid()) {
+                $soon = new LimitIterator($varsity, 0, $upcomingLimit);
+                printItems($soon, 'Varsity');
+                print $varsity->valid() ? '<li data-icon="false" class="ul-li--view-more"><a href="#all-varsity" title="view all upcoming varsity events">view all</a></li>' : '';
+            }
 
-		} else {
-			print '<p>No upcoming events found</p>';
-		}
+            if ($jv->valid()) {
+                $soon = new LimitIterator($jv, 0, $upcomingLimit);
+                printItems($soon, 'JV');
+                print $jv->valid() ? '<li data-icon="false" class="ul-li--view-more"><a href="#all-jv" title="view all upcoming jv events">view all</a></li>' : '';
+            }
+
+           print '</ul>';
+        } else {
+            print '<p>No upcoming events found</p>';
+        }
 		?>
 	</div><!-- /content -->
 
 </div><!-- /page -->
+
+<!-- All Varsity -->
+<div data-role="page" data-theme="b" id="all-varsity">
+    <div data-role="header" data-theme="b">
+        <a href="#soon" title="soon" data-icon="back" data-iconpos="notext" data-direction="reverse">back</a>
+        <a href="add-event-type-chooser.php" title="add event" class="ui-btn-right" data-icon="plus" data-iconpos="notext" data-rel="dialog">add</a>
+        <h1>All Upcoming Varsity Events</h1>
+    </div><!-- /header -->
+
+    <div data-role="content">
+        <?php
+        $varsity->rewind();
+        if ($varsity->valid()) {
+            print '<ul data-role="listview" data-theme="d" data-divider-theme="d" data-split-theme="d" data-split-icon="gear">';
+            if ($varsity->valid()) {
+                printItems($varsity, 'Varsity');
+            }
+            print '</ul>';
+        } else {
+            print '<p>No upcoming varisty events found</p>';
+        }
+        ?>
+    </div><!-- /content -->
+</div>
+
+<!-- All JV -->
+<div data-role="page" data-theme="b" id="all-jv">
+    <div data-role="header" data-theme="b">
+        <a href="#soon" title="soon" data-icon="back" data-iconpos="notext" data-direction="reverse">back</a>
+        <a href="add-event-type-chooser.php" title="add event" class="ui-btn-right" data-icon="plus" data-iconpos="notext" data-rel="dialog">add</a>
+        <h1>All Upcoming JV Events</h1>
+    </div><!-- /header -->
+
+    <div data-role="content">
+        <?php
+        $jv->rewind();
+        if ($jv->valid()) {
+            print '<ul data-role="listview" data-theme="d" data-divider-theme="d" data-split-theme="d" data-split-icon="gear">';
+            if ($jv->valid()) {
+                printItems($jv, 'JV');
+            }
+            print '</ul>';
+        } else {
+            print '<p>No upcoming jv events found</p>';
+        }
+        ?>
+    </div><!-- /content -->
+</div>
+
 
 </body>
 </html>
